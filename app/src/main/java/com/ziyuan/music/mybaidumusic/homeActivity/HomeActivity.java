c636bb1bc4a4b3092ed8aba126a61a1f68d06162
dllo <dllo@yizhui.local>
2017-01-03 09:38:15 +0800

    2017.1.3我的BaiduMusic第一弹,加油

---
+++ app/src/main/java/com/ziyuan/music/mybaidumusic/homeActivity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.ziyuan.music.mybaidumusic.homeActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ziyuan.music.mybaidumusic.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
